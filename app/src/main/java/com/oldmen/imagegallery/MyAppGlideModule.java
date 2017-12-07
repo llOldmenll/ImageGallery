deb9df13d0fd14e3b83abb347a520856877899a8
llOldmenll <shark7733@gmail.com>
2017-12-07 22:17:19 +0200

    - check

--- app/src/main/java/com/oldmen/imagegallery/MyAppGlideModule.java
+++ app/src/main/java/com/oldmen/imagegallery/MyAppGlideModule.java
@@ -3,5 +3,6 @@ package com.oldmen.imagegallery;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
 
+
 @GlideModule
 public class MyAppGlideModule extends AppGlideModule {}
